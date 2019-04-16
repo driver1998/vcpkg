8fd3c530575b46568de80a19b5c6eb34058813c8
driver1998 <driver1998@foxmail.com>
2019-04-16 18:36:19 +0800

    glib port update for windows-arm64

--- ports/glib/portfile.cmake
+++ ports/glib/portfile.cmake
@@ -27,6 +27,7 @@ vcpkg_extract_source_archive_ex(
     REF ${GLIB_VERSION}
     PATCHES
         use-libiconv-on-windows.patch
+        arm64-defines.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
